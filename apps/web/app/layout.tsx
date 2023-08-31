4680a69bb72ce4c227e5c4ffd76f8a47a8d8f129
luannguyenbkit <luannguyenbkit@gmail.com>
2023-08-31 09:39:13 +0700

    Add shadcn ui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./global.css";
+import "./globals.css";
 
 export default function RootLayout({
   children,
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,14 +6,16 @@ export default async function Page() {
   const allPosts = await prisma.post.findMany();
 
   return (
-    <div className="h-full flex justify-center items-center">
+    <div className="h-full flex flex-col justify-center items-center">
       <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
         CODE FOR STARTUP.
       </h1>
 
-      {allPosts.map((post) => (
-        <div key={post.id}>{post.title}</div>
-      ))}
+      <div>
+        {allPosts.map((post) => (
+          <div key={post.id}>{post.title}</div>
+        ))}
+      </div>
     </div>
   );
 }
