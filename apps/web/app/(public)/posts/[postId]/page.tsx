2c153ca15f3b40cbd7be15ed3182ce63c0da7b92
luannguyenbkit <luannguyenbkit@gmail.com>
2023-10-22 20:16:47 +0700

    Refactor format

--- apps/web/app/(public)/posts/[postId]/page.tsx
+++ apps/web/app/(public)/posts/[postId]/page.tsx
@@ -1,6 +1,6 @@
 import { getPostById } from "app/post-actions"
 
-import PostDetail from "@/molecules/posts/post-detai"
+import PostDetail from "@/molecules/posts/post-detail"
 
 export default async function Page({ params }: { params: { postId: string } }) {
   const post = await getPostById(Number(params?.postId))
