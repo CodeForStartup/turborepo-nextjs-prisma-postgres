04f8c2b26e4c52af97c2ddb755946423792dfa1f
luannguyenbkit <luannguyenbkit@gmail.com>
2023-11-12 13:55:26 +0700

    Add tags page

--- apps/web/app/(public-leftsidebar)/layout.tsx
+++ apps/web/app/(public-leftsidebar)/layout.tsx
@@ -1,12 +1,8 @@
-import Image from "next/image"
-
 export default async function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid w-full grid-cols-12 md:gap-8">
       <div className="hide md:col-span-4">
-        <div className="rounded-sm bg-white">
-          <Image alt="" src="../" width={200} height={200} />
-        </div>
+        <div className="rounded-sm bg-white"></div>
       </div>
       <div className="col-span-12 md:col-span-8">{children}</div>
     </div>
