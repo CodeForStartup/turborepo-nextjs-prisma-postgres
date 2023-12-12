9bd106818215d8e47562e5052c23ab457152ee52
Luan Nguyen <luan.nguyen@jitera.com>
2023-12-12 21:53:31 +0700

    Contact form

--- apps/web/app/(public)/contact/page.tsx
+++ apps/web/app/(public)/contact/page.tsx
@@ -1,3 +1,12 @@
+import ContactForm from "@/molecules/contact-form/contact-form"
+import PageTitle from "@/molecules/page-title"
+
 export default async function ContactUs() {
-  return <div className="grid w-full grid-cols-12 gap-8">Contact</div>
+  return (
+    <div className="grid w-full">
+      <PageTitle title="Contact us" description="Contact us" />
+
+      <ContactForm />
+    </div>
+  )
 }
