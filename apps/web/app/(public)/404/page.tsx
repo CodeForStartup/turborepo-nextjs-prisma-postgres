3c8eec218883434b795131bbed73c0ba7a9aab4e
Luan Nguyen <luan.nguyen@jitera.com>
2023-12-15 23:57:39 +0700

    Add 404 page

---
+++ apps/web/app/(public)/404/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
