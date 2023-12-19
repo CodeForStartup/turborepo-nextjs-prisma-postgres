d8340eff72424a44fb4f55e4a3fcf8e452390c4f
Luan Nguyen <luan.nguyen@jitera.com>
2023-12-19 10:06:15 +0700

    Update post api

--- apps/web/@/molecules/Editor/menu-item.tsx
+++ apps/web/@/molecules/Editor/menu-item.tsx
@@ -9,7 +9,7 @@ type MenuItemProps = {
 
 const MenuItem = ({ icon, title, action, isActive = null }: MenuItemProps) => (
   <button
-    className={`menu-item border border-gray-500 text-gray-500 hover:bg-gray-500 hover:bg-gray-900 hover:text-gray-100 ${
+    className={`menu-item border border-gray-500 text-gray-500 hover:bg-gray-500 hover:text-gray-100 ${
       isActive && isActive() ? "is-active" : ""
     }`}
     onClick={action}
