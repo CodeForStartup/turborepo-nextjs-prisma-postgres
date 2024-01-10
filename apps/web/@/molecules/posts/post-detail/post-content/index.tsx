49f959d79c56b5cd6594f53345eabde1e46368e6
Luan Nguyen <luannguyenbkit@gmail.com>
2024-01-10 23:03:15 +0700

    Update page render

--- apps/web/@/molecules/posts/post-detail/post-content/index.tsx
+++ apps/web/@/molecules/posts/post-detail/post-content/index.tsx
@@ -1,14 +1,78 @@
 import React from "react"
-import reactHtmlParser from "react-html-parser"
+import htmlReactParser, { attributesToProps, domToReact } from "html-react-parser"
 
+import APP_ROUTES from "@/constants/routes"
 import { TPostItem } from "@/types/posts"
+import { generatePath } from "@/utils/generatePath"
 
 interface PostContentProps {
   post: TPostItem
 }
 
 const PostContent: React.FC<PostContentProps> = ({ post }) => {
-  return <div className="post-content mt-8">{reactHtmlParser(post?.content)}</div>
+  const options = {
+    replace: (domNode) => {
+      if (domNode.name === "h1") {
+        const props = attributesToProps(domNode.attribs)
+        return (
+          <h1 {...props}>
+            <a
+              href={generatePath(APP_ROUTES.POST, {
+                postId: post?.slug || post?.id,
+              })}
+            >
+              1
+            </a>
+            {domNode.children[0].data}
+          </h1>
+        )
+      }
+      if (domNode.name === "h2") {
+        const props = attributesToProps(domNode.attribs)
+        return (
+          <h2 {...props} id={domNode.children[0].data}>
+            <a
+              href={`${generatePath(APP_ROUTES.POST, {
+                postId: post?.slug || post?.id,
+              })}#${domNode.children[0].data}`}
+            >
+              {domNode.children[0].data}
+            </a>
+          </h2>
+        )
+      }
+      if (domNode.name === "h3") {
+        const props = attributesToProps(domNode.attribs)
+        return (
+          <h3 {...props} id={domNode.children[0].data}>
+            <a
+              href={`${generatePath(APP_ROUTES.POST, {
+                postId: post?.slug || post?.id,
+              })}#${domNode.children[0].data}`}
+            >
+              {domNode.children[0].data}
+            </a>
+          </h3>
+        )
+      }
+      if (domNode.name === "h4") {
+        const props = attributesToProps(domNode.attribs)
+        return (
+          <h4 {...props} id={domNode.children[0].data}>
+            <a
+              href={`${generatePath(APP_ROUTES.POST, {
+                postId: post?.slug || post?.id,
+              })}#${domNode.children[0].data}`}
+            >
+              {domToReact(domNode.children)}
+            </a>
+          </h4>
+        )
+      }
+    },
+  }
+
+  return <div className="post-content mt-8">{htmlReactParser(post?.content, options)}</div>
 }
 
 export default PostContent
--- apps/web/app/(public-fullwidth)/posts/[postId]/page.tsx
+++ apps/web/app/(public-fullwidth)/posts/[postId]/page.tsx
@@ -1,7 +1,5 @@
-import { useEffect } from "react"
 import { Metadata } from "next"
 import { notFound } from "next/navigation"
-import * as tocbot from "tocbot"
 
 import APP_APIS from "@/constants/apis"
 import PostDetail from "@/molecules/posts/post-detail"
