eab2eab90b454b7d0af670196b13d3462acefcaf
Luan Nguyen <luannguyenbkit@gmail.com>
2024-01-12 22:25:13 +0700

    Update TOC

--- apps/web/@/molecules/posts/post-detail/table-of-contents/index.tsx
+++ apps/web/@/molecules/posts/post-detail/table-of-contents/index.tsx
@@ -15,10 +15,18 @@ const TableOfContents: React.FC = () => {
       headingSelector: "h1, h2, h3, h4, h5, h6",
       // For headings inside relative or absolute positioned containers within content.
       hasInnerContainers: true,
+      // positionFixedSelector: ".tocbot",
+      includeHtml: true,
+      includeTitleTags: false,
+      onClick: (e) => {
+        // console.log("you clicked a link", e)
+      },
+      headingsOffset: 100,
+      scrollSmoothOffset: 0,
     })
   }, [])
 
-  return <div className="tocbot">{/* Render table of contents here */}</div>
+  return <div className="tocbot sticky top-[56px]">{/* Render table of contents here */}</div>
 }
 
 export default TableOfContents
