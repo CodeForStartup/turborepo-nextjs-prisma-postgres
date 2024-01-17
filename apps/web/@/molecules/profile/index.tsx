4cc22c0c687599089e1fdad6e75282a78e920679
Luan Nguyen <luannguyenbkit@gmail.com>
2024-01-17 22:23:54 +0700

    Update profile (cont)

--- apps/web/@/molecules/profile/index.tsx
+++ apps/web/@/molecules/profile/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from "react"
-import { Form, useForm } from "react-hook-form"
+import { useForm } from "react-hook-form"
 
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
@@ -17,34 +17,57 @@ const Profile = () => {
   const onSubmit = () => {}
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mt-8">
-        <Typography variant="h2">Account</Typography>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mt-8">
+          <Typography variant="h2">Account</Typography>
 
-        <div className="mb-4">
-          <Label htmlFor="email">Username</Label>
-          <Input id="username" {...register("username")} />
-        </div>
-
-        <div className="mb-4 flex gap-4">
-          <div className="flex-1">
-            <Label htmlFor="firstName">First name</Label>
-            <Input id="firstName" {...register("firstName")} />
+          <div className="mb-4">
+            <Label htmlFor="email">Username</Label>
+            <Input id="username" {...register("username")} />
           </div>
-          <div className="flex-1">
-            <Label htmlFor="lastName">Last name</Label>
-            <Input id="lastName" {...register("lastName")} />
+
+          <div className="mb-4 flex gap-4">
+            <div className="flex-1">
+              <Label htmlFor="firstName">First name</Label>
+              <Input id="firstName" {...register("firstName")} />
+            </div>
+            <div className="flex-1">
+              <Label htmlFor="lastName">Last name</Label>
+              <Input id="lastName" {...register("lastName")} />
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" {...register("email")} />
           </div>
         </div>
 
-        <div className="mb-4">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" {...register("email")} />
-        </div>
-      </div>
+        <div>
+          <Typography variant="h2">Password</Typography>
 
-      <div></div>
-    </form>
+          <div className="mb-4">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" {...register("password")} />
+          </div>
+
+          <div className="mb-4">
+            <Label htmlFor="confirmPassword">Confirm password</Label>
+            <Input id="confirmPassword" {...register("confirmPassword")} />
+          </div>
+        </div>
+
+        <div>
+          <Typography variant="h2">DeActive account</Typography>
+
+          <div className="mb-4">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" {...register("password")} />
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
