04993aca3fab074522df173931824db8d8b6f93e
Luan Nguyen <luannguyenbkit@gmail.com>
2024-01-20 21:52:44 +0700

    Update profile UI

--- apps/web/@/molecules/profile/index.tsx
+++ apps/web/@/molecules/profile/index.tsx
@@ -3,49 +3,87 @@
 import React from "react"
 import { useForm } from "react-hook-form"
 
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
 import Typography from "../typography"
 
 const Profile = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm()
+  const { register, handleSubmit } = useForm()
 
-  const onSubmit = () => {}
+  const onSubmit = () => {
+    // TODO: implement
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mt-8">
+        <div className="mt-8 rounded-md border p-8">
           <Typography variant="h2">Account</Typography>
 
           <div className="mb-4">
-            <Label htmlFor="email">Username</Label>
-            <Input id="username" {...register("username")} />
+            <Label className="pb-2" htmlFor="email">
+              Username
+            </Label>
+            <Input disabled id="username" {...register("username")} />
           </div>
 
           <div className="mb-4 flex gap-4">
             <div className="flex-1">
               <Label htmlFor="firstName">First name</Label>
-              <Input id="firstName" {...register("firstName")} />
+              <Input disabled id="firstName" {...register("firstName")} />
             </div>
             <div className="flex-1">
               <Label htmlFor="lastName">Last name</Label>
-              <Input id="lastName" {...register("lastName")} />
+              <Input disabled id="lastName" {...register("lastName")} />
             </div>
           </div>
 
           <div className="mb-4">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" {...register("email")} />
+            <Input disabled id="email" {...register("email")} />
+          </div>
+
+          <div className="mb-4">
+            <Label htmlFor="phoneNumber">Phone number</Label>
+            <Input id="phoneNumber" {...register("phoneNumber")} />
+          </div>
+
+          <div className="mb-4">
+            <Label htmlFor="address">Twitter</Label>
+            <Input id="address" {...register("address")} />
+          </div>
+
+          <div className="mb-4">
+            <Label htmlFor="address">Github</Label>
+            <Input id="address" {...register("address")} />
+          </div>
+
+          <div className="mb-4">
+            <Label htmlFor="address">Facebook</Label>
+            <Input id="address" {...register("address")} />
+          </div>
+
+          <div className="mb-4">
+            <Label htmlFor="address">Bio</Label>
+            <Textarea id="address" {...register("address")} />
+          </div>
+
+          <div className="w-full">
+            <Button className="w-full" type="submit">
+              Update your account
+            </Button>
           </div>
         </div>
 
-        <div>
+        <div className="mt-8 rounded-md border p-8">
           <Typography variant="h2">Password</Typography>
+
+          <div className="mb-4">
+            <Label htmlFor="password">Current password</Label>
+            <Input id="password" {...register("password")} />
+          </div>
 
           <div className="mb-4">
             <Label htmlFor="password">Password</Label>
@@ -58,13 +96,12 @@ const Profile = () => {
           </div>
         </div>
 
-        <div>
-          <Typography variant="h2">DeActive account</Typography>
+        <div className="mt-8 rounded-md border p-8">
+          <Typography variant="h2">Deactive account</Typography>
 
-          <div className="mb-4">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" {...register("password")} />
-          </div>
+          <Button variant="destructive" className="mt-4" type="button">
+            Deactive account
+          </Button>
         </div>
       </form>
     </div>
--- apps/web/app/(protected)/user/profile/page.tsx
+++ apps/web/app/(protected)/user/profile/page.tsx
@@ -4,7 +4,7 @@ import Profile from "@/molecules/profile"
 export default function Page() {
   return (
     <div>
-      <PageTitle title="Profile" description="" />
+      <PageTitle title="Profile" description="User detail information" />
 
       <Profile />
     </div>
