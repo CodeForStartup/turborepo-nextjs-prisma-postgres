6576a5ba2de3ca35d019448376be85e461cce2c1
Luan Nguyen <luannguyenbkit@gmail.com>
2024-01-22 20:55:55 +0700

    Update sign in page

--- apps/web/@/molecules/sign-in-form/index.tsx
+++ apps/web/@/molecules/sign-in-form/index.tsx
@@ -1,12 +1,17 @@
 "use client"
 
 import { Label } from "@radix-ui/react-dropdown-menu"
+import { Tabs } from "@radix-ui/react-tabs"
 import { GithubIcon } from "lucide-react"
 import { signIn } from "next-auth/react"
+import Link from "next/link"
 import { useSearchParams } from "next/navigation"
 
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Typography from "../typography"
 
 export default function SignInForm() {
   const searchParams = useSearchParams()
@@ -20,36 +25,102 @@ export default function SignInForm() {
   }
 
   return (
-    <div className="mt-16 flex w-full flex-1 justify-center rounded-md bg-white p-20 md:max-w-[500px]">
-      <div className="grid-6 grid w-full max-w-md">
+    <div className="mt-16 w-full max-w-md flex-1 rounded-md p-8">
+      <div>
+        <Typography variant="h1">Sign In</Typography>
+
+        <Typography variant="span" className="mt-4">
+          Sign in to your account to continue.
+        </Typography>
+      </div>
+
+      <div className="grid-6 mt-8 grid w-full">
         <form>
-          <div className="grid gap-2">
-            <div className="grid gap-1">
-              <Label className="sr-only">Email</Label>
-              <Input
-                id="email"
-                placeholder="name@example.com"
-                type="email"
-                autoCapitalize="none"
-                autoComplete="email"
-                autoCorrect="off"
-              />
-            </div>
-            <Button>Sign In with Email</Button>
-          </div>
+          <Card>
+            <CardContent className="pt-6">
+              <Tabs>
+                <div className="mb-1">
+                  <Label>Sign in mode</Label>
+                </div>
+                <TabsList className="grid w-full grid-cols-2">
+                  <TabsTrigger value="magic">Magic Link</TabsTrigger>
+                  <TabsTrigger value="username_password">Username/Password</TabsTrigger>
+                </TabsList>
+                <TabsContent value="username_password">
+                  <div className="grid gap-4">
+                    <div className="grid gap-1">
+                      <Label className="">Email</Label>
+                      <Input
+                        id="email"
+                        placeholder="name@example.com"
+                        type="email"
+                        autoCapitalize="none"
+                        autoComplete="email"
+                        autoCorrect="off"
+                      />
+                    </div>
+                    <div className="grid gap-1">
+                      <Label className="">Password</Label>
+                      <Input
+                        id="password"
+                        placeholder="********"
+                        type="password"
+                        autoCapitalize="none"
+                        autoCorrect="off"
+                      />
+                    </div>
+                    <Button>Sign In</Button>
+                  </div>
+                </TabsContent>
+                <TabsContent value="magic">
+                  <div className="grid gap-4">
+                    <div className="grid gap-1">
+                      <Label className="">Email</Label>
+                      <Input
+                        id="email"
+                        placeholder="name@example.com"
+                        type="email"
+                        autoCapitalize="none"
+                        autoComplete="email"
+                        autoCorrect="off"
+                      />
+                    </div>
+                    <Button>Sign In With Email</Button>
+                  </div>
+                </TabsContent>
+              </Tabs>
+            </CardContent>
+            <CardFooter>
+              <div className="flex w-full flex-col">
+                <div className="relative">
+                  <div className="absolute inset-0 flex items-center">
+                    <span className="w-full border-t" />
+                  </div>
+                  <div className="relative flex justify-center py-4 text-xs uppercase">
+                    <span className="bg-background px-2 text-muted-foreground">
+                      Or continue with
+                    </span>
+                  </div>
+                </div>
+                <Button variant="outline" type="button" onClick={onSignIn}>
+                  <GithubIcon size={16} />
+                  <span className="ml-2">GitHub</span>
+                </Button>
+              </div>
+            </CardFooter>
+          </Card>
         </form>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center py-4 text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
-          </div>
-        </div>
-        <Button variant="outline" type="button" onClick={onSignIn}>
-          <GithubIcon size={16} />
-          <span className="ml-2">GitHub</span>
-        </Button>
+      </div>
+
+      <div className="mt-4 text-center">
+        <Link href="signup">
+          <Typography variant="span" className="mt-4">
+            Don&apos;t have an account?{" "}
+            <Typography className="font-bold hover:underline" variant="span">
+              Sign Up
+            </Typography>
+          </Typography>
+        </Link>
       </div>
     </div>
   )
