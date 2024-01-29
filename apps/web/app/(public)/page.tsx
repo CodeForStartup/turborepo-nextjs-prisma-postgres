fcbe3f3adb5e26869e30a16b108f115ef24d20cc
Luan Nguyen <luannguyenbkit@gmail.com>
2024-01-29 22:27:26 +0700

    Update orderBy for /posts api

--- apps/web/app/(public)/page.tsx
+++ apps/web/app/(public)/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next"
+import querystring from "qs"
 
+import APP_APIS from "@/constants/apis"
 import Filter from "@/molecules/home/filter"
 import PostItem from "@/molecules/posts/post-item"
 import { GetDataSuccessType } from "@/types"
@@ -10,14 +12,29 @@ export const metadata: Metadata = {
   description: "Top of the best things in the world",
 }
 
-export default async function Page() {
-  const posts = await fetch(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/public/posts`, {
-    method: "GET",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const { filter, period } = searchParams
+
+  const posts = await fetch(
+    `${process.env.NEXT_PUBLIC_FRONTEND_URL}${APP_APIS.public.posts.GET}?${querystring.stringify({
+      query: searchParams?.query,
+      limit: searchParams?.limit,
+      page: searchParams?.page,
+      filter: filter || "lasted",
+      period: period || "",
+    })}`,
+    {
+      method: "GET",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
   const postsJson: GetDataSuccessType<TPostItem[]> = await posts.json()
 
   return (
