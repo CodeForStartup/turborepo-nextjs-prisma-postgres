78d1b8bbb9c8f83e912e623229e7bcbed4740d92
Luan Nguyen <luannguyenbkit@gmail.com>
2024-02-02 23:11:29 +0700

    Update locale path

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,9 +1,31 @@
+import { match } from "@formatjs/intl-localematcher"
+import Negotiator from "negotiator"
 import { getToken } from "next-auth/jwt"
 import { NextRequest, NextResponse } from "next/server"
+
+const locales = ["en-US", "nl-NL", "nl"]
+
+function getLocale(req: NextRequest) {
+  const languages = new Negotiator({ headers: req.headers }).languages()
+  const defaultLocale = "en-US"
+
+  match(languages, locales, defaultLocale) // -> 'en-US'
+}
 
 export async function middleware(req: NextRequest) {
   const currentPathname = req.nextUrl.pathname
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  const { pathname } = req.nextUrl
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  )
+
+  if (pathnameHasLocale) return
+
+  // Redirect if there is no locale
+  const locale = getLocale(req)
+  req.nextUrl.pathname = `/${locale}${pathname}`
 
   if (!session?.email && currentPathname.startsWith("/user")) {
     const newUrl = req.nextUrl.clone()
@@ -17,5 +39,5 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(newUrl)
   }
 
-  return NextResponse.next()
+  return NextResponse.next(req)
 }
