97455b3039817a775e1a8d8484ba4ab0a6a913d3
Luan Nguyen <luannguyenbkit@gmail.com>
2024-02-04 22:42:46 +0700

    Add next-intl

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,51 +1,29 @@
-import { match } from "@formatjs/intl-localematcher"
-import Negotiator from "negotiator"
 import { getToken } from "next-auth/jwt"
+import createIntlMiddleware from "next-intl/middleware"
 import { NextRequest, NextResponse } from "next/server"
 
-import APP_ROUTES from "@/constants/routes"
-
-const locales = ["en", "ja", "vi", "zh", "fr"]
-const defaultLocale = "en"
-
-function getLocale(req: NextRequest) {
-  const languages = new Negotiator({
-    headers: {
-      "accept-language": req.headers.get("accept-language"),
-    },
-  }).languages()
-
-  return match(languages, locales, defaultLocale) || defaultLocale
-}
+const handleI18nRouting = createIntlMiddleware({
+  locales: ["en", "de"],
+  defaultLocale: "en",
+})
 
 export async function middleware(req: NextRequest) {
   const currentPathname = req.nextUrl.pathname
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
-  const { pathname } = req.nextUrl
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  )
-
-  let locale = defaultLocale
-  if (!pathnameHasLocale) {
-    locale = getLocale(req)
-    req.nextUrl.pathname = `/${locale}${pathname}`
-  }
-
   if (!session?.email && currentPathname.startsWith("/user")) {
     const newUrl = req.nextUrl.clone()
     const currentSearchParam = newUrl.searchParams.toString()
-    newUrl.pathname = APP_ROUTES.LOGIN
+    newUrl.pathname = "/signIn"
     newUrl.searchParams.set(
       "callbackUrl",
       encodeURIComponent(`${currentPathname}?${currentSearchParam}`)
     )
 
-    return NextResponse.rewrite(newUrl.toString())
+    return NextResponse.redirect(newUrl)
   }
 
-  return NextResponse.rewrite(req.nextUrl)
+  return handleI18nRouting(req)
 }
 
 export const config = {
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui", "database"],
 };
+
+module.exports =
+  withNextIntl(nextConfig);
+
