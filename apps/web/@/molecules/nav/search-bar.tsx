7946b6641d8563fcdda18d3eda0f195f6e6f454c
Luan Nguyen <luannguyenbkit@gmail.com>
2024-02-11 22:54:47 +0700

    Update search bar

--- apps/web/@/molecules/nav/search-bar.tsx
+++ apps/web/@/molecules/nav/search-bar.tsx
@@ -1,30 +1,83 @@
 "use client"
 
-// import { useTranslations } from "next-intl"
-import { useRouter, useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
 
 import { useTranslations } from "next-intl"
 
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import {
+  CommandDialog,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import { cn } from "@/lib/utils"
 
 export default function SearchBar() {
-  const router = useRouter()
-  const searchParams = useSearchParams()
   const t = useTranslations()
+  const [open, setOpen] = useState(true)
+
+  useEffect(() => {
+    const down = (e) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
+
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  }, [])
 
   return (
-    <div className="w-[300px]">
-      <Input
-        className="w-[300px]"
-        placeholder={t("common.search_placeholder")}
-        defaultValue={searchParams.get("query")}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault()
-            router.push(`/search?query=${e.currentTarget.value}`)
-          }
-        }}
-      />
+    <div className="">
+      <Button
+        variant="outline"
+        className={cn(
+          "relative h-8 w-full justify-start rounded-[0.5rem] bg-background text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64"
+        )}
+        onClick={() => setOpen(true)}
+      >
+        <span className="inline-flex">{t("common.searchPlaceholder")}</span>
+        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+      <CommandDialog
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <CommandInput placeholder={t("common.searchPlaceholder")} />
+        <CommandList>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <span>Launch</span>
+            </CommandItem>
+          </CommandGroup>
+          {/*   <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <span>Mail</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup> */}
+        </CommandList>
+      </CommandDialog>
     </div>
   )
 }
