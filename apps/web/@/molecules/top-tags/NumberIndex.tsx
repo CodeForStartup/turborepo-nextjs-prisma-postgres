0ad3248b4a0b634b3830cc3e35df9caddbc557bf
Luan Nguyen <luannguyenbkit@gmail.com>
2024-03-23 15:14:08 +0700

    Update search UI (cont)

---
+++ apps/web/@/molecules/top-tags/NumberIndex.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import React from "react"
+
+import { useTheme } from "next-themes"
+
+import { bebasNeue } from "@/font"
+import { cn } from "@/lib/utils"
+
+interface NumberIndexProps {
+  number: number
+}
+
+const NumberIndex: React.FC<NumberIndexProps> = ({ number }) => {
+  const { theme } = useTheme()
+
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-center text-2xl font-extrabold text-transparent",
+        bebasNeue.className
+      )}
+      style={{
+        WebkitTextStroke: theme === "light" ? "0.5px #000" : "0.5px #fff",
+      }}
+    >
+      {number}.
+    </div>
+  )
+}
+
+export default NumberIndex
