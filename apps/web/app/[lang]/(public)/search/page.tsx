16b35b8adfaf21d7314ca6ce5c26075996cec092
Luan Nguyen <luannguyenbkit@gmail.com>
2024-03-23 11:55:09 +0700

    Update search UI (cont)

--- apps/web/app/[lang]/(public)/search/page.tsx
+++ apps/web/app/[lang]/(public)/search/page.tsx
@@ -1,15 +1,20 @@
 import { Metadata } from "next"
 
 import { getPosts } from "@/actions/public/posts"
+import Filter from "@/molecules/home/filter"
 import SearchBar from "@/molecules/nav/search-bar"
 import NoItemFounded from "@/molecules/no-item-founded"
 import PostItem from "@/molecules/posts/post-item"
 
-export const metadata: Metadata = {
-  title: "Search",
-  description: "Search",
+export async function generateMetadata({ searchParams }): Promise<Metadata> {
+  return {
+    title: `${searchParams?.search} - Search result`,
+    description: `Search result for "${searchParams?.search}`,
+  }
 }
 
+// TODO: Hight light matching
+// TODO: Load more
 export default async function Page({ searchParams }) {
   const posts = await getPosts({
     searchParams,
@@ -28,6 +33,9 @@ export default async function Page({ searchParams }) {
               <span className="px-2 text-2xl text-slate-900">{`"${searchParams?.search}"`}</span>
             </h1>
           </div>
+
+          <Filter />
+
           <div className="mt-4">
             {posts?.data?.map((post) => (
               <PostItem
