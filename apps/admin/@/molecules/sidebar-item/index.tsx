dd8ea9d1ae9326deec2e2bcd0e0903e5d7faa547
thang.cai <thang.cai@tiki.vn>
2024-04-02 23:43:30 +0700

    add sidebar

--- apps/admin/@/molecules/sidebar-item/index.tsx
+++ apps/admin/@/molecules/sidebar-item/index.tsx
@@ -1,24 +1,101 @@
 "use client"
+import * as React from "react"
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 import { buttonVariants } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { ChevronUp, ChevronDown } from "lucide-react"
 
 import Typography from "../typography"
 
-export type SidebarItemProps = {
+type SidebarItemChildrenProps = {
   label: string
   link: string
   icons: React.ReactElement
 }
 
-export default function SidebarItem({ label, link, icons }: SidebarItemProps) {
+export type SidebarItemProps = {
+  label: string
+  link: string
+  icons: React.ReactElement
+  children: Array<SidebarItemChildrenProps>
+}
+
+export default function SidebarItem({ label, link, icons, children }: SidebarItemProps) {
   const currentPathname = usePathname()
+  const [open, setOpen] = React.useState(false);
 
   const isActive =
     currentPathname === "/" ? link === "/" : currentPathname.startsWith(link) && link !== "/"
+
+  if (children) {
+    return (
+      <Collapsible open={open} onOpenChange={setOpen}>
+        <div className="flex">
+          <div
+            className={cn(
+              buttonVariants({ variant: "ghost", size: "sm" }),
+              "flex-1 cursor-pointer items-center justify-between dark:text-white dark:hover:bg-muted dark:hover:text-white",
+              {
+                "bg-accent": isActive,
+              }
+            )}
+          >
+            <div className={'flex flex-row'}>
+              {icons && <div className="mr-2 flex h-6 w-4 items-center justify-center">{icons}</div>}
+              <Typography
+                variant="span"
+                className={cn({
+                  "font-bold": isActive,
+                })}
+              >
+                {label}
+              </Typography>
+            </div>
+
+            <CollapsibleTrigger asChild>
+              <button className="IconButton">{open ? <ChevronUp strokeWidth={3} size={18} /> : <ChevronDown strokeWidth={3} size={18} />}</button>
+            </CollapsibleTrigger>
+          </div>
+
+        </div>
+
+        <CollapsibleContent>
+          {children?.map((item, key) => {
+            return (
+              <Link
+                href={item?.link}
+                key={key}
+                className={cn(
+                  buttonVariants({ variant: "ghost", size: "sm" }),
+                  "ml-5 dark:text-white dark:hover:bg-muted dark:hover:text-white",
+                  {
+                    "bg-accent": isActive,
+                  }
+                )}
+              >
+                <Typography
+                  variant="span"
+                  className={cn({
+                    "font-bold": isActive,
+                  })}
+                >
+                  {item?.label}
+                </Typography>
+              </Link>
+            )
+          })}
+        </CollapsibleContent>
+      </Collapsible>
+    )
+  }
 
   return (
     <Link
--- apps/admin/app/[lang]/layout.tsx
+++ apps/admin/app/[lang]/layout.tsx
@@ -11,14 +11,51 @@ import "remixicon/fonts/remixicon.css"
 import { NextIntlClientProvider, useMessages } from "next-intl"
 
 import Footer from "@/molecules/footer"
-import SidebarItem from "@/molecules/sidebar-item"
+import SidebarItem, { SidebarItemProps } from "@/molecules/sidebar-item"
 import { Providers } from "@/providers"
-
+import { User, HomeIcon, StickyNote, TagIcon, Settings } from "lucide-react"
 export const metadata = {
   icons: {
     icon: "/assets/logo.png",
   },
 }
+
+const SIDE_BAR = [
+  {
+    label: "Dashboard",
+    link: "/user/profile",
+    icons: <HomeIcon size={16} />,
+  },
+  {
+    label: "Posts",
+    link: "/user/posts",
+    icons: <StickyNote size={16} />,
+  },
+  {
+    label: "Tags",
+    link: "/user/tags",
+    icons: <TagIcon size={16} />,
+  },
+  {
+    label: "Users",
+    link: "/user/user",
+    icons: <User size={16} />,
+  },
+  {
+    label: "Setting",
+    icons: <Settings size={16} />,
+    children: [
+      {
+        label: "Permission and Role",
+        link: "/user/Permission",
+      },
+      {
+        label: "Configuration",
+        link: "/user/configuration",
+      },
+    ]
+  },
+] as Array<SidebarItemProps>
 
 export default function RootLayout({
   params: { lang },
@@ -43,7 +80,14 @@ export default function RootLayout({
             <Providers>
               <Nav />
               <main className="flex min-h-[800px]">
-                <div className="w-[300px] border-r">SIDE BAR</div>
+                <div className="w-[250px] border-r p-1">
+                  {SIDE_BAR.map((item) => (
+                    <SidebarItem
+                      key={item.label}
+                      {...item}
+                    />
+                  ))}
+                </div>
                 {children}
                 <ToastContainer />
               </main>
