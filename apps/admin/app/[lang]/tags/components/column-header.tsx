c9e310d6dd5040e41ce9680ec9282bc64e7c1e3c
Luan Nguyen <luannguyenbkit@gmail.com>
2024-04-13 21:28:10 +0700

    Update columm header (cont)

--- apps/admin/app/[lang]/tags/components/column-header.tsx
+++ apps/admin/app/[lang]/tags/components/column-header.tsx
@@ -1,4 +1,3 @@
-import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons"
 import { Column } from "@tanstack/react-table"
 
 import { cn } from "@/lib/utils"
@@ -20,14 +19,7 @@ export function DataTableColumnHeader<TData, TValue>({
   return (
     <div className={cn("flex items-center space-x-2", className)}>
       <span>{title}</span>
-      <div className="flex flex-col items-center">
-        <ArrowUpIcon
-          className={cn("h-3 w-3", column.isSortedDesc ? "text-gray-400" : "text-gray-600")}
-        />
-        <ArrowDownIcon
-          className={cn("h-3 w-3", column.isSortedDesc ? "text-gray-600" : "text-gray-400")}
-        />
-      </div>
+      <div className="flex flex-col items-center"></div>
     </div>
   )
 }
