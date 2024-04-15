2b4bd11546c5801c1313b79b9bd921e2a1b55d14
Luan Nguyen <luannguyenbkit@gmail.com>
2024-04-15 10:15:09 +0700

    Update get tags pagination

--- apps/admin/@/components/ui/checkbox.tsx
+++ apps/admin/@/components/ui/checkbox.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { Check } from "lucide-react"
 
@@ -18,9 +19,7 @@ const Checkbox = React.forwardRef<
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
-    >
+    <CheckboxPrimitive.Indicator className={cn("flex items-center justify-center text-current")}>
       <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
