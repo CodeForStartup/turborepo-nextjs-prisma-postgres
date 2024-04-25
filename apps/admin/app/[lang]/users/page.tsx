c51c0d038163225567a6e8ca16aefc892fa02496
Luan Nguyen <luannguyenbkit@gmail.com>
2024-04-25 22:42:10 +0700

    Init user page

--- apps/admin/app/[lang]/users/page.tsx
+++ apps/admin/app/[lang]/users/page.tsx
@@ -1,7 +1,21 @@
 import React from "react"
 
+import PageTitle from "@/molecules/page-title"
+
+export const metadata = {
+  title: "Users",
+  description: "You can create, edit, and delete users here.",
+}
+
 const Users: React.FC = () => {
-  return <div className="flex justify-center p-8">Users</div>
+  return (
+    <div className="w-full p-8">
+      <PageTitle
+        title="Users"
+        description="You can create, edit, and delete users here."
+      />
+    </div>
+  )
 }
 
 export default Users
