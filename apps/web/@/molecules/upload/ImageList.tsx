0ff2ec7719a9920abe46f9d0842fac3483e10125
Luan Nguyen <luannguyenbkit@gmail.com>
2024-08-15 22:02:24 +0700

    feat: allow to select/de-select image

--- apps/web/@/molecules/upload/ImageList.tsx
+++ apps/web/@/molecules/upload/ImageList.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import Image from "next/image"
 
 import { Image as ImageType } from "database"
-import { TrashIcon } from "lucide-react"
+import { Check, Circle, TrashIcon } from "lucide-react"
 import { Button } from "ui"
 
 type ImageListProps = {
@@ -10,28 +10,47 @@ type ImageListProps = {
 }
 
 const ImageList: React.FC<ImageListProps> = ({ images }) => {
+  const [selectedImage, setSelectedImage] = useState<ImageType | null>(null)
+
   return (
-    <div className="mt-2 grid grid-cols-5 gap-3">
+    <div className="mt-2 flex flex-wrap gap-3">
       {images?.map((image) => (
         <div
           key={image.id}
-          className="group relative"
+          className="group relative border"
         >
           <Image
             src={`${process.env.NEXT_PUBLIC_FRONTEND_URL}${image.url}`}
-            alt={`Image ${image.id}`}
-            width={90}
-            height={90}
-            className="h-[90px] w-[90px] cursor-pointer object-cover"
-            // onClick={() => handleSelect(image.id)}
+            alt={`Image ${image.name}`}
+            width={160}
+            height={160}
+            className="cursor-pointe h-[160px] w-[160px] bg-gray-300 object-cover"
           />
           <Button
             variant="destructive"
             size="icon"
-            className="absolute right-1 top-1 opacity-0 transition-opacity group-hover:opacity-100"
-            // onClick={() => handleDelete(image.id)}
+            className="absolute bottom-1 right-1 h-7 w-7 rounded-full opacity-0 transition-opacity group-hover:opacity-100"
+            onClick={() => console.log("delete")}
           >
             <TrashIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="absolute right-1 top-1 h-7 w-7 rounded-full p-0"
+            onClick={() => {
+              console.log(image)
+
+              setSelectedImage(image)
+            }}
+          >
+            {selectedImage?.id === image.id ? (
+              <Check
+                strokeWidth={1.5}
+                className="h-6 w-6 text-blue-500"
+              />
+            ) : (
+              <Circle className="h-6 w-6" />
+            )}
           </Button>
         </div>
       ))}
