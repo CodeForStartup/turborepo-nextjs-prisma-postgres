97aee263e7fc99eb38e3677754fe6cb83eeb706d
Luan Nguyen <luannguyenbkit@gmail.com>
2024-09-03 21:46:03 +0700

    feat: scroll to load more posts

---
+++ apps/web/@/molecules/post-list/index.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useCallback, useState } from "react"
+import { useSearchParams } from "next/navigation"
+
+import { getPosts, PostStatus, TPostItem } from "database"
+
+import useInfiniteScroll from "@/hooks/useInfinityScroll"
+
+import PostItem from "../posts/post-item"
+
+export default function PostList() {
+  const searchParams = useSearchParams()
+  const [page, setPage] = useState(1)
+  const [hasMore, setHasMore] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [posts, setPosts] = useState<TPostItem[]>([])
+
+  const loadMoreBooks = useCallback(async () => {
+    setLoading(true)
+    const newPosts = await getPosts({
+      ...searchParams,
+      postStatus: PostStatus.PUBLISHED,
+      page: page.toString(),
+    })
+    setPosts((prevPosts) => [...prevPosts, ...newPosts?.data?.data])
+    setHasMore(page < newPosts?.data?.pagination?.totalPages)
+    setLoading(false)
+  }, [page, searchParams])
+
+  const { setNode } = useInfiniteScroll(loadMoreBooks, null, loading)
+
+  return (
+    <div>
+      {posts?.map((post) => (
+        <PostItem
+          key={post.id}
+          post={post}
+        />
+      ))}
+
+      <div
+        ref={setNode}
+        className="h-10 w-full"
+      >
+        {loading && <div>Loading...</div>}
+      </div>
+    </div>
+  )
+}
